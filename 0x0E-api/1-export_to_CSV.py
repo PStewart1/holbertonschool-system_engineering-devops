9c23c17655382986d83717f5b31014f91a40d3b6
PStewart1 <4920@holbertonstudents.com>
2022-11-30 19:33:34 -0600

    added functionality from task 0

--- 0x0E-api/1-export_to_CSV.py
+++ 0x0E-api/1-export_to_CSV.py
@@ -10,13 +10,24 @@ if __name__ == "__main__":
     eid = sys.argv[1]
     name_response = requests.get(
         "https://jsonplaceholder.typicode.com/users/" + eid).json()
-    name = name_response.get('username')
+    name = name_response.get("username")
     response = requests.get(
         "https://jsonplaceholder.typicode.com/users/" + eid + "/todos").json()
     todos = []
+    total = len(response)
+    done = 0
     for v in response:
-        todo = [eid, name, v.get('completed'), v.get('title')]
-        todos.append(todo)
+        if v.get("completed") is True:
+            done += 1
+            todos.append(v.get("title"))
+    print("Employee {} is done with tasks({}/{}):".format(name, done, total))
+    for i in todos:
+        print("\t {}".format(i))
+
+    todos_csv = []
+    for v in response:
+        todo = [eid, name, v.get("completed"), v.get("title")]
+        todos_csv.append(todo)
     file = eid + ".csv"
     with open(file, 'w', newline='') as csvfile:
         my_writer = csv.writer(csvfile, quoting=1)
