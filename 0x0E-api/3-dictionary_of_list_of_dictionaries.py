70380ab923e79f8facc5a30a5c96785caca1fada
PStewart1 <4920@holbertonstudents.com>
2022-11-30 21:15:10 -0600

    Records all tasks from all employees, in json

---
+++ 0x0E-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""script uses a public api, returns information
+about todo list progressfor all users. """
+import requests
+import sys
+import json
+
+
+if __name__ == "__main__":
+    user_list = requests.get(
+        "https://jsonplaceholder.typicode.com/users/").json()
+    todos = requests.get(
+        "https://jsonplaceholder.typicode.com/todos").json()
+    users = {}
+    for v in user_list:
+        users.update({v.get('id'): v.get('username')})
+
+    json_dic = {}
+    todo_list = []
+    user_id = 1
+    for v in todos:
+        if v.get('userId') > user_id:
+            json_dic.update({user_id: todo_list})
+            user_id = v.get('userId')
+            todo_list = []
+        todo = {"username": users.get(v.get('userId')), "task": v.get('title'),
+                "completed": v.get('completed')}
+        todo_list.append(todo)
+    json_dic.update({user_id: todo_list})
+
+    with open("todo_all_employees.json", 'w') as f:
+        json.dump(json_dic, f)
