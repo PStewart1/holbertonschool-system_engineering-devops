feb8058803895e6ee6b42e1ef66ca66f938be15d
PStewart1 <4920@holbertonstudents.com>
2022-11-30 18:41:45 -0600

    script dumps request into csv file, no quotes

---
+++ 0x0E-api/1-export_to_CSV.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""script uses a public api, for a given employee ID,
+returns information about his/her TODO list progress. """
+import requests
+import sys
+import csv
+
+
+if __name__ == "__main__":
+    eid = sys.argv[1]
+    name_response = requests.get(
+        "https://jsonplaceholder.typicode.com/users/" + eid).json()
+    name = name_response['username']
+    response = requests.get(
+        "https://jsonplaceholder.typicode.com/users/" + eid + "/todos").json()
+    todos = []
+    for v in response:
+        todo = [eid, name, v['completed'], v['title']]
+        todos.append(todo)
+    file = eid + ".csv"
+    with open (file,'w',newline = '') as csvfile:
+        my_writer = csv.writer(csvfile)   
+        my_writer.writerows(todos)
